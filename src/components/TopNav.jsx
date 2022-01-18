179ccf34e9105a2ac001854c8101bf7bb4483592
Will McComis <wmccomis@nd.edu>
2022-01-18 16:41:44 +0000

    changed the navigation to stay at top

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,7 +5,8 @@ import {Link, animateScroll as scroll} from "react-scroll"
 const TopNav = () => {
   return (
 
-    <nav className="navbar-expand-lg navbar-light navbar-custom sticky-top">
+    // <nav className="navbar-expand-lg navbar-light navbar-custom sticky-top">
+    <nav className="navbar-expand-lg navbar-light navbar-custom">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
