a39955d7e97bd8371417f19cabfbdcd79d9e95e5
Will McComis <wmccomis@nd.edu>
2022-01-21 11:11:18 +0000

    created navbar background and made progress on the scrolling natures

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../assets/stylesheets/topnav.scss'
-import {Link, animateScroll as scroll} from "react-scroll"
+import {Link, animateScroll} from "react-scroll"
+import $ from 'jquery';
 
 const TopNav = () => {
   return (
-
-    // <nav className="navbar-expand-lg navbar-light navbar-custom sticky-top">
-    <nav className="navbar-expand-lg navbar-light navbar-custom">
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+    <Link className="navbar-brand" smooth={true} to="home" duration={500}>Will McComis</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -17,9 +17,17 @@ const TopNav = () => {
       <Link className="nav-link" activeClass="nav-link active" smooth={true} to="flying" duration={500}>Flying</Link>
       <Link className="nav-link" activeClass="nav-link active" smooth={true} to="resume" duration={500}>Resume</Link>
     </div>
-</nav>
+  </nav>
   )
 }
 
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".navbar");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// });
+   
 
 export default TopNav
+
